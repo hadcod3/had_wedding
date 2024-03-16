bb127a061208e33edf397c277c76b2fb13b53241
Nur Hadi <decoding0506@gmail.com>
2024-03-17 04:20:25 +0700

    fixed error

--- app/api/webhook/stripe/route.ts
+++ app/api/webhook/stripe/route.ts
@@ -25,7 +25,7 @@ export async function POST(request: Request) {
 
     const order = {
       stripeId: id,
-      eventId: metadata?.eventId || '',
+      packetId: metadata?.packetId || '',
       buyerId: metadata?.buyerId || '',
       totalAmount: amount_total ? (amount_total / 100).toString() : '0',
       createdAt: new Date(),
@@ -37,3 +37,4 @@ export async function POST(request: Request) {
 
   return new Response('', { status: 200 })
 }
+event

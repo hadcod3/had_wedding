426fd18a89561d9756b406aa8acf4b9690a30ccc
Nur Hadi <decoding0506@gmail.com>
2024-04-01 02:32:42 +0700

    fixed error

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -33,7 +33,7 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
             buyerId: order.buyerId,
         },
         mode: 'payment',
-        success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
+        success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/profile`,
         cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/`,
         });
 
